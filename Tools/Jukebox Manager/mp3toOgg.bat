d2c850e9a4965c8e4213dcd589b0d4d96eba5a10
cosmosgc <cosmoskitsune@hotmail.com>
2025-02-09 22:33:59 -0300

    Update ao app
    *Fila, agora vc pode enviar mais arquivos de uma vez
    *Id agora substitui espaços por _
    Um novo py e bat para converter mp3 para ogg

---
+++ Tools/Jukebox Manager/mp3toOgg.bat
@@ -0,0 +1,12 @@
+@echo off
+
+:: Ensure pip is up to date
+python -m pip install --upgrade pip
+
+:: Install required dependencies
+pip install -r requirements.txt
+
+:: Run the script
+call python mp3toOgg.py
+
+pause
